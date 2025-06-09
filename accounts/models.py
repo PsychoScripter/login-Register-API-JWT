5f03d3ea3897b53cff0915843568a5e47a0aefa4
Hesam <psychoscripter1@gmail.com>
2025-06-09 12:36:21 +0330

    Creat CustomUser in models.py

---
+++ accounts/models.py
@@ -0,0 +1,14 @@
+from django.contrib.auth.models import AbstractUser
+from django.db import models
+
+# Create your models here.
+class User(AbstractUser):
+    first_name = models.CharField(max_length=120)
+    last_name = models.CharField(max_length=120)
+    username = models.CharField(max_length=120)
+    phone = models.CharField(max_length=120)
+    password = models.CharField(max_length=120)
+    email = models.EmailField(unique=True, null=True)
+
+    def __str__(self):
+        return self.username
